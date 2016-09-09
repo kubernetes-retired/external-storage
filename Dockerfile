cedceab6943a80279785b646834e5fc468b48e08
Matthew Wong <mawong@redhat.com>
2016-09-09 18:52:55 -0400

    Split run_nfs.sh into start & stop

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,9 @@ RUN yum -y install /usr/bin/ps nfs-utils && yum clean all
 RUN mkdir -p /exports
 
 # expose mountd 20048/tcp and nfsd 2049/tcp
-EXPOSE 2049/tcp 20048/tcp
+EXPOSE 2049/tcp 20048/tcp 111/tcp 111/udp
 
-COPY nfs nfs
-COPY run_nfs.sh run_nfs.sh
-ENTRYPOINT ["/nfs"]
+COPY nfs-provisioner nfs-provisioner
+COPY start_nfs.sh start_nfs.sh
+COPY stop_nfs.sh stop_nfs.sh
+ENTRYPOINT ["/nfs-provisioner"]
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/golang/glog"
@@ -15,27 +19,44 @@ import (
 func main() {
 	flag.Parse()
 
-	cmd := exec.Command("/run_nfs.sh")
-	err := cmd.Start()
-	if err != nil {
-		glog.Fatalf("Error starting run_nfs.sh: %v", err)
-	}
-
+	startAndLog("/start_nfs.sh")
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		err := cmd.Wait()
-		glog.Fatalf("run_nfs.sh stopped: %v", err)
+		<-c
+		startAndLog("/stop_nfs.sh")
+		os.Exit(1)
 	}()
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		glog.Fatalf("failed to create config: %v", err)
+		glog.Fatalf("Failed to create config: %v", err)
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		glog.Fatalf("failed to create client: %v", err)
+		glog.Fatalf("Failed to create client: %v", err)
 	}
 	nc := newNfsController(clientset, 5*time.Second)
 
 	nc.Run(wait.NeverStop)
+}
+
+func startAndLog(command string) {
+	cmd := exec.Command(command)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		glog.Errorf("Stdout pipe error: %v", err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		glog.Fatalf("Error starting %s: %v", command, err)
+	}
+	in := bufio.NewScanner(stdout)
+	for in.Scan() {
+		glog.Errorf("%s: %v", command, in.Text())
+	}
+	if err := in.Err(); err != nil {
+		glog.Errorf("Scanner error: %v", err)
+	}
 }
