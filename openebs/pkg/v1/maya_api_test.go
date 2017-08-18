cd40fe96709fd979fe416cdabc9a56b3350a36c0
Satyam Zode <satyamzode@gmail.com>
2017-08-18 17:39:04 +0530

    Update Boilerplate for openebs-provisioner files

--- openebs/pkg/v1/maya_api_test.go
+++ openebs/pkg/v1/maya_api_test.go
@@ -1,6 +1,5 @@
 /*
 Copyright 2016-2017 The Kubernetes Authors.
-Copyright 2016-2017 The OpenEBS Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package v1
 
 import (
