0f8dbeeb3f77dc099268a7255d21d2f4527e3c81
wackxu <xushiwei5@huawei.com>
2018-03-20 09:55:37 +0800

    update generate shell

--- hack/update-generated-code.sh
+++ hack/update-generated-code.sh
@@ -14,18 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ROOT=$(unset CDPATH && cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
-cd $ROOT
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-source "${ROOT}/hack/lib.sh"
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-hack::build_generators
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+  github.com/kubernetes-incubator/external-storage/snapshot/pkg/client github.com/kubernetes-incubator/external-storage/snapshot/pkg/apis \
+  volumesnapshot:v1 \
+  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
 
-ALL_FQ_APIS=github.com/kubernetes-incubator/external-storage/snapshot/pkg/apis/crd/v1
-
-echo "Generating deepcopy funcs"
-${OUTPUT_HOSTBIN}/deepcopy-gen --input-dirs ${ALL_FQ_APIS} -O zz_generated.deepcopy --bounding-dirs $ALL_FQ_APIS --logtostderr
+# To use your own boilerplate text use:
+#   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
