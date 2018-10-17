358b392554c9cb205fe7922c803c9f8f27f8227e
Matthew Wong <mawong@redhat.com>
2018-10-17 15:05:31 -0400

    Use lib util instead of upstream helper

--- flex/pkg/volume/provision.go
+++ flex/pkg/volume/provision.go
@@ -19,11 +19,11 @@ package volume
 import (
 	"github.com/golang/glog"
 	"github.com/kubernetes-sigs/sig-storage-lib-external-provisioner/controller"
+	"github.com/kubernetes-sigs/sig-storage-lib-external-provisioner/util"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/kubernetes/pkg/volume/util"
 	"k8s.io/utils/exec"
 	"strconv"
 )
